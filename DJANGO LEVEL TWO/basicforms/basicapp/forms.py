04499ac5b7624acf2cefc7cb9e8f9bea080370df
Ananya0610 <ananyashukla0610@gmail.com>
2022-01-23 00:03:36 +0530

    Major Commit

--- DJANGO LEVEL TWO/basicforms/basicapp/forms.py
+++ DJANGO LEVEL TWO/basicforms/basicapp/forms.py
@@ -3,6 +3,15 @@ from django.core import validators
 
 class FormName(forms.Form):
     name=forms.CharField()
-    email=forms.CharField()
+    email=forms.EmailField()
+    vmail=forms.EmailField(label="ENTER AGAIN!")
     text=forms.CharField(widget=forms.Textarea)
-    botcatcher=forms.CharField(required=False,widget=forms.HiddenInput,validators=[validators.MaxLengthValidator(0)])
+
+
+    def clean(self):
+        all_cleaned_data=super().clean()
+        email=all_cleaned_data['email']
+        vmail=all_cleaned_data['vmail']
+
+        if email!=vmail:
+            raise forms.ValidationError("make sure emails match")
